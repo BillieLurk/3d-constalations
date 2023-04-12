c88414269cb23f4e8fa420b6dc8b724056b0a651
BillieLurk <bill@1618holding.com>
2023-04-12 19:26:12 +0200

    idk

--- src/node.js
+++ src/node.js
@@ -1,212 +1,275 @@
-import * as THREE from 'three';
-import { Scene } from 'three';
+import * as THREE from "three";
+import { MeshLine, MeshLineMaterial, MeshLineRaycast } from "three.meshline";
 
-
-import { MeshLine, MeshLineMaterial, MeshLineRaycast } from 'three.meshline';
-
-const maxConnections = 4;
-const maxConnectionDist = 5;
+const maxConnections = 1000;
+const maxConnectionDist = 3;
 const textScale = 0.002;
 
-const bounds = 3;
+const bounds = Infinity;
 //create a node class with three js objects
 class Node extends THREE.Object3D {
-    constructor(pos, size, label, color, velMult = 0.001) {
-        super();
-        this.connectedNodes = [];
+  constructor(pos, size, label, color, velMult = 0.001) {
+    super();
+    this.connectedNodes = [];
+    this.ogPos = pos;
+    this.color = color || 0xaaaaaa;
+    this.isSelected = false;
+    this.velocity = new THREE.Vector3(
+      Math.random() * 0.1 - 0.05,
+      Math.random() * 0.1 - 0.05,
+      Math.random() * 0.1 - 0.05
+    );
+    this.velocity.multiplyScalar(velMult);
+
+    this.size = size;
+    this.label = label === "" ? null : label;
+    //get distance from origin
+
+    const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
+    const sphereMaterial = new THREE.MeshBasicMaterial({
+      color: this.color,
+      transparent: true,
+      opacity: 0.0,
+    });
+    this.sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    this.sphereMesh.scale.set(size, size, size);
+    this.sphereMesh.renderOrder = 1000;
+
+    
 
 
-        this.color = color || 0xaaaaaa;
+    this.add(this.sphereMesh2);
 
-        this.velocity = new THREE.Vector3(Math.random() * 0.1 - 0.05, Math.random() * 0.1 - 0.05, Math.random() * 0.1 - 0.05);
-        this.velocity.multiplyScalar(velMult);
+    // Create text sprite
+    if (this.label !== null) {
+      const textTexture = new THREE.CanvasTexture(createTextLabel(this.label));
+      const textMaterial = new THREE.SpriteMaterial({ map: textTexture });
+      this.textSprite = new THREE.Sprite(textMaterial);
+      this.textSprite.scale.set(
+        textTexture.image.width * textScale,
+        textTexture.image.height * textScale,
+        1
+      ); // Scale text
+      this.textSprite.position.set(
+        0,
+        this.sphereMesh.geometry.parameters.radius - 0.5,
+        0
+      ); // Position text relative to sphere
+    }
+    // Initialize child nodes and lines array
+    this.childNodes = [];
+    this.lines = [];
 
-        this.size = size;
-        this.label = label === "" ? null : label;
-        //get distance from origin
+    // Create text container object and add sphere and text to it
 
-        const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
-        const sphereMaterial = new THREE.MeshBasicMaterial({ color: this.color });
-        this.sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
-        this.sphereMesh.scale.set(size, size, size);
+    this.add(this.sphereMesh);
+    if (this.label !== null) {
+      this.add(this.textSprite);
+    }
+    // Set position of container object
+    this.position.copy(pos);
+  }
 
-        // Create text sprite
-        if (this.label !== null) {
-            const textTexture = new THREE.CanvasTexture(createTextLabel(this.label));
-            const textMaterial = new THREE.SpriteMaterial({ map: textTexture });
-            this.textSprite = new THREE.Sprite(textMaterial);
-            this.textSprite.scale.set(textTexture.image.width * textScale, textTexture.image.height * textScale, 1); // Scale text
-            this.textSprite.position.set(0, this.sphereMesh.geometry.parameters.radius - 0.5, 0); // Position text relative to sphere
-        }
-        // Initialize child nodes and lines array
-        this.childNodes = [];
-        this.lines = [];
+  updateTextLabel(label) {
+    this.remove(this.textSprite); // Remove old text sprite
+    const textTexture = new THREE.CanvasTexture(createTextLabel(label)); // Create new canvas texture
+    const newTextMaterial = new THREE.SpriteMaterial({
+      map: textTexture,
+      alphaTest: 0.001,
+    });
+    const newTextSprite = new THREE.Sprite(newTextMaterial);
+    newTextSprite.scale.set(
+      textTexture.image.width * textScale,
+      textTexture.image.height * textScale,
+      1
+    ); // Scale text
+    newTextSprite.position.set(
+      0,
+      this.sphereMesh.geometry.parameters.radius - 0.8,
+      0
+    ); // Position text relative to sphere
+    this.add(newTextSprite); // Add new text sprite to container
+    this.textSprite = newTextSprite; // Update reference to current text sprite
+  }
 
-        // Create text container object and add sphere and text to it
-        this.nodeObject = new THREE.Object3D();
-        //this.nodeObject.add(this.sphereMesh);
-        if (this.label !== null) {
-            this.nodeObject.add(this.textSprite);
-        }
-        // Set position of container object
-        this.nodeObject.position.copy(pos);
-
-        // Add text container to scene
-        this.add(this.nodeObject);
-
-
+  tick() {
+    this.position.add(this.velocity);
+    
+    //make node spring toward ogPos and have variables for dampening and spring strength
+    if (!this.isSelected) {
+        const springStrength = 0.05;
+        const springDampening = 0.85;
+        const distance = this.ogPos.distanceTo(this.position);
+        const spring = this.ogPos
+          .clone()
+          .sub(this.position)
+          .multiplyScalar(springStrength * distance);
+        this.velocity.add(spring);
+        this.velocity.multiplyScalar(springDampening);
     }
 
-    updateTextLabel(label) {
-        this.nodeObject.remove(this.textSprite); // Remove old text sprite
-        const textTexture = new THREE.CanvasTexture(createTextLabel(label)); // Create new canvas texture
-        const newTextMaterial = new THREE.SpriteMaterial({ map: textTexture, alphaTest: 0.001 });
-        const newTextSprite = new THREE.Sprite(newTextMaterial);
-        newTextSprite.scale.set(textTexture.image.width * textScale, textTexture.image.height * textScale, 1); // Scale text
-        newTextSprite.position.set(0, this.sphereMesh.geometry.parameters.radius - 0.8, 0); // Position text relative to sphere
-        this.nodeObject.add(newTextSprite); // Add new text sprite to container
-        this.textSprite = newTextSprite; // Update reference to current text sprite
+    if (this.label !== null) {
+      this.distance = Math.sqrt(
+        this.position.x * this.position.x +
+          this.position.y * this.position.y +
+          this.position.z * this.position.z
+      );
+      this.label = this.distance.toFixed(6);
+      this.updateTextLabel(this.label);
     }
 
-    tick() {
+    // Update lines to child nodes
+    this.childNodes.forEach((childNode, index) => {
+      const distance = this.position.distanceTo(childNode.position);
+      this.lines[index].setPoints([
+        new THREE.Vector3(0, 0, 0),
+        childNode.position.clone().sub(this.position),
+      ]);
 
-        this.nodeObject.position.add(this.velocity);
+      if (distance > maxConnectionDist) {
+        this.disconnect(childNode);
+        childNode.disconnect(this);
+      }
+    });
 
-        if (this.label !== null) {
-            this.distance = Math.sqrt(this.nodeObject.position.x * this.nodeObject.position.x + this.nodeObject.position.y * this.nodeObject.position.y + this.nodeObject.position.z * this.nodeObject.position.z);
-            this.label = this.distance.toFixed(6);
-            this.updateTextLabel(this.label);
+    // prevent nodes from going out of bounds
+    if (this.position.x < -bounds || this.position.x > bounds) {
+      this.velocity.x *= -1;
+    }
+    if (this.position.y < -bounds || this.position.y > bounds) {
+      this.velocity.y *= -1;
+    }
+    if (this.position.z < -bounds || this.position.z > bounds) {
+      this.velocity.z *= -1;
+    }
+  }
+
+  //getNearestNodes Function with parameter of nodes array and returns the nearest nodes in order of distance
+  getNearestNodes(nodes) {
+    let nearestNodes = [];
+    let distances = [];
+
+    nodes.forEach((node) => {
+      if (node !== this) {
+        const distance = this.position.distanceTo(node.position);
+        if (distance < maxConnectionDist) {
+          distances.push(distance);
+          nearestNodes.push(node);
         }
+      }
+    });
 
-
-        // Update lines to child nodes
-        this.childNodes.forEach((childNode, index) => {
-            const distance = this.nodeObject.position.distanceTo(childNode.nodeObject.position);
-            this.lines[index].setPoints([this.nodeObject.position, childNode.nodeObject.position]);
-
-            if (distance > maxConnectionDist) {
-                this.disconnect(childNode);
-                childNode.disconnect(this);
-            }
-        });
-
-        // prevent nodes from going out of bounds
-        if (this.nodeObject.position.x < -bounds || this.nodeObject.position.x > bounds) {
-            this.velocity.x *= -1;
+    // Sort nodes by distance
+    for (const [i, distance] of distances.entries()) {
+      for (const [j, _] of distances.slice(i + 1).entries()) {
+        if (distances[j] < distances[j - 1]) {
+          // Swap distances
+          [distances[j], distances[j - 1]] = [distances[j - 1], distances[j]];
+          // Swap nodes
+          [nearestNodes[j], nearestNodes[j - 1]] = [
+            nearestNodes[j - 1],
+            nearestNodes[j],
+          ];
         }
-        if (this.nodeObject.position.y < -bounds || this.nodeObject.position.y > bounds) {
-            this.velocity.y *= -1;
-        }
-        if (this.nodeObject.position.z < -bounds || this.nodeObject.position.z > bounds) {
-            this.velocity.z *= -1;
-        }
+      }
     }
 
-    //getNearestNodes Function with parameter of nodes array and returns the nearest nodes in order of distance
-    getNearestNodes(nodes) {
-        let nearestNodes = [];
-        let distances = [];
+    return nearestNodes;
+  }
 
-        nodes.forEach(node => {
-            if (node !== this) {
-                const distance = this.nodeObject.position.distanceTo(node.nodeObject.position);
-                if (distance < maxConnectionDist) {
-                    distances.push(distance);
-                    nearestNodes.push(node);
-                }
-            }
-        });
+  applyForce(force) {
+    this.velocity.add(force);
+  }
 
-        // Sort nodes by distance
-        for (const [i, distance] of distances.entries()) {
-            for (const [j, _] of distances.slice(i + 1).entries()) {
-                if (distances[j] < distances[j - 1]) {
-                    // Swap distances
-                    [distances[j], distances[j - 1]] = [distances[j - 1], distances[j]];
-                    // Swap nodes
-                    [nearestNodes[j], nearestNodes[j - 1]] = [nearestNodes[j - 1], nearestNodes[j]];
-                }
-            }
+  connect(childNode) {
+    if (
+      !this.isConnected(childNode) &&
+      !childNode.isConnected(this) &&
+      this.childNodes.length < maxConnections
+    ) {
+      this.childNodes.push(childNode);
+
+      const line = new MeshLine();
+      line.setGeometry(
+        new THREE.BufferGeometry().setFromPoints([
+          this.position,
+          childNode.position,
+        ])
+      );
+
+      const material = new MeshLineMaterial({
+        color: new THREE.Color(this.color),
+        lineWidth: 0.01,
+      });
+      material.transparent = true;
+      const mesh = new THREE.Mesh(line, material);
+
+      this.lines.push(line);
+
+      this.add(mesh);
+    }
+  }
+
+  disconnect(childNode) {
+    const index = this.childNodes.indexOf(childNode);
+
+    if (index > -1) {
+      const line = this.lines[index];
+      const mesh = this.children.find(
+        (child) => child.geometry === line.geometry
+      );
+
+      // Remove the line from the arrays
+      this.childNodes.splice(index, 1);
+      this.lines.splice(index, 1);
+
+      // Remove the mesh from the scene
+      if (mesh) {
+        this.remove(mesh);
+        if (line && line.material) {
+          line.material.dispose();
         }
-
-        return nearestNodes;
-    }
-
-    connect(childNode) {
-        if (!this.isConnected(childNode) && !childNode.isConnected(this) && this.childNodes.length < maxConnections) {
-            this.childNodes.push(childNode);
-
-            const line = new MeshLine();
-            line.setGeometry(new THREE.BufferGeometry().setFromPoints([this.nodeObject.position, childNode.nodeObject.position]));
-
-            const material = new MeshLineMaterial({ color: new THREE.Color(this.color), lineWidth: 0.01 });
-            material.transparent = true;
-            const mesh = new THREE.Mesh(line, material);
-
-            this.lines.push(line);
-
-            this.add(mesh);
+        if (mesh.geometry) {
+          mesh.geometry.dispose();
         }
-    }
-
-    disconnect(childNode) {
-        const index = this.childNodes.indexOf(childNode);
-
-        if (index > -1) {
-            const line = this.lines[index];
-            const mesh = this.children.find(child => child.geometry === line.geometry);
-
-            // Remove the line from the arrays
-            this.childNodes.splice(index, 1);
-            this.lines.splice(index, 1);
-
-            // Remove the mesh from the scene
-            if (mesh) {
-                this.remove(mesh);
-                if (line && line.material) {
-                    line.material.dispose();
-                }
-                if (mesh.geometry) {
-                    mesh.geometry.dispose();
-                }
-                if (mesh.material) {
-                    mesh.material.dispose();
-                }
-            }
-
-            // Dispose of the line geometry
-            if (line && line.geometry) {
-                line.geometry.dispose();
-            }
+        if (mesh.material) {
+          mesh.material.dispose();
         }
-    }
+      }
 
-    isConnected(childNode) {
-        return this.childNodes.includes(childNode);
+      // Dispose of the line geometry
+      if (line && line.geometry) {
+        line.geometry.dispose();
+      }
     }
+  }
 
-    getConnectedNodes() {
-        return this.connectedNodes;
-    }
+  isConnected(childNode) {
+    return this.childNodes.includes(childNode);
+  }
+
+  getConnectedNodes() {
+    return this.connectedNodes;
+  }
 }
 
 export default Node;
 
 function createTextLabel(label) {
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
-    const fontSize = 48;
-    const font = `${fontSize}px Arial`;
-    context.font = font;
-    const textWidth = context.measureText(label).width;
-    const width = textWidth + fontSize * 8; // Add some padding to the width
-    const height = fontSize * 1.5;
-    canvas.width = width;
-    canvas.height = height;
-    context.font = font;
-    context.textAlign = 'left';
-    context.textBaseline = 'middle';
-    context.fillStyle = 'rgba(21,21,21,0.8)';
-    context.fillText(label, width / 2, height / 2);
-    return canvas;
+  const canvas = document.createElement("canvas");
+  const context = canvas.getContext("2d");
+  const fontSize = 48;
+  const font = `${fontSize}px Arial`;
+  context.font = font;
+  const textWidth = context.measureText(label).width;
+  const width = textWidth + fontSize * 8; // Add some padding to the width
+  const height = fontSize * 1.5;
+  canvas.width = width;
+  canvas.height = height;
+  context.font = font;
+  context.textAlign = "left";
+  context.textBaseline = "middle";
+  context.fillStyle = "rgba(21,21,21,0.8)";
+  context.fillText(label, width / 2, height / 2);
+  return canvas;
 }
--- src/script.js
+++ src/script.js
@@ -1,26 +1,26 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
-import Node from './node'
-import { Vector3 } from 'three'
-import { MeshLine, MeshLineMaterial } from 'three.meshline'
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "dat.gui";
+import Node from "./node";
+import { Vector3 } from "three";
+import { MeshLine, MeshLineMaterial } from "three.meshline";
+
 
 //settings
 const nodeCount = 25;
 
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI();
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 scene.background = new THREE.Color(0xffffff);
 scene.fog = new THREE.Fog(0xffffff, 0, 20);
-
 
 //grid plane object
 const gridHelper = new THREE.GridHelper(100, 100, 0x999999, 0xaaaaaa);
@@ -31,21 +31,20 @@ scene.add(gridHelper);
 //create x nodes at random positions
 const nodes = [];
 for (let i = 0; i < nodeCount; i++) {
-    const x = Math.random() * 6 - 3;
-    const y = Math.random() * 6 - 3;
-    const z = Math.random() * 6 - 3;
-    const size = Math.random() * 0.1 + 0.1;
-    const label = i;
-    if (i % 2 == 0) {
-        const node = new Node(new Vector3(x, y, z), size, "", 0xaaaaaa, 0);
-        nodes.push(node);
-        scene.add(node);
-    } else {
-        const node = new Node(new Vector3(x, y, z), size, label, 0xaaaaff, 0.1);
-        nodes.push(node);
-        scene.add(node);
-    }
-
+  const x = Math.random() * 6 - 3;
+  const y = Math.random() * 6 - 3;
+  const z = Math.random() * 6 - 3;
+  const size = 0.3;
+  const label = i;
+  if (i % 2 == 0) {
+    const node = new Node(new Vector3(x, y, z), size, "", 0xaaaaaa, 0);
+    nodes.push(node);
+    scene.add(node);
+  } else {
+    const node = new Node(new Vector3(x, y, z), size, label, 0xaaaaff, 0);
+    nodes.push(node);
+    scene.add(node);
+  }
 }
 /*
 //randomly connect nodes
@@ -56,102 +55,169 @@ for (let i = 0; i < 10; i++) {
 }
 */
 
-
-
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+const pointLight = new THREE.PointLight(0xffffff, 0.1);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () => {
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 10
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 4;
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    antialias: true,
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  antialias: true,
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
 
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
 let points, line, mesh;
 points = [];
 
+// Raycaster
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
 
+// Helper variables
+let selectedNode = null;
+let selectedNodeOffset = null;
+
+// Mouse event listeners
+const onMouseMove = (event) => {
+  // Calculate mouse position in normalized device coordinates
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  // If a node is selected, update its position
+  if (selectedNode) {
+    // Raycast from camera to mouse position
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObject(selectedNode, true);
+
+    if (intersects.length > 0) {
+      // Calculate the new position of the selected node based on the ray intersection point
+      const point = intersects[0].point.sub(selectedNodeOffset);
+      // Check if the y-coordinate of the selected node's position is below a certain threshold
+      if (point.y > -1.5) {
+        selectedNode.position.copy(point);
+      }
+    }
+  }
+};
+
+const onMouseDown = (event) => {
+  // Raycast from camera to mouse position
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObjects(nodes, true);
+
+  if (intersects.length > 0) {
+    if (intersects[0].object.type == "Mesh") {
+      // Select the first node that was intersected by the ray
+      selectedNode = intersects[0].object.parent;
+      selectedNode.isSelected = true;
+
+      // Calculate the offset between the ray intersection point and the center of the selected node
+      selectedNodeOffset = intersects[0].point.sub(selectedNode.position);
+    }
+  }
+};
+
+const onMouseUp = (event) => {
+  // Deselect the selected node
+  selectedNode.isSelected = false;
+  selectedNode = null;
+};
+//add keybaord event listener
+window.addEventListener("keydown", (event) => {
+  if (event.key == " ") {
+    //turn of orbit controls
+    controls.enabled = !controls.enabled;
+  }
+});
+
+window.addEventListener("mousemove", onMouseMove, false);
+window.addEventListener("mousedown", onMouseDown, false);
+window.addEventListener("mouseup", onMouseUp, false);
 
 const tick = () => {
-
-    //connect the 3 nearest nodes dont connect to self or already connected nodes otherwise connect to the next nearest node
-    for (let i = 0; i < nodes.length; i++) {
-        const node = nodes[i];
-        const nearestNodes = node.getNearestNodes(nodes);
-        for (let j = 0; j < nearestNodes.length; j++) {
-            const nearestNode = nearestNodes[j];
-            if (node !== nearestNode && !node.isConnected(nearestNode)) {
-                node.connect(nearestNode);
-                break;
-            }
-        }
+  //connect the 3 nearest nodes dont connect to self or already connected nodes otherwise connect to the next nearest node
+  for (let i = 0; i < nodes.length; i++) {
+    const node = nodes[i];
+    const nearestNodes = node.getNearestNodes(nodes);
+    for (let j = 0; j < nearestNodes.length; j++) {
+      const nearestNode = nearestNodes[j];
+      if (node !== nearestNode && !node.isConnected(nearestNode)) {
+        node.connect(nearestNode);
+        break;
+      }
     }
+  }
 
-    const elapsedTime = clock.getElapsedTime()
+  const elapsedTime = clock.getElapsedTime();
 
-    nodes.forEach(node => {
-        node.tick();
-    });
+  nodes.forEach((node) => {
+    node.tick();
+  });
 
-    // Update Orbital Controls
-    controls.update()
+  // Update Orbital Controls
 
-    // Render
-    renderer.render(scene, camera)
+  controls.update();
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  // Render
+  renderer.render(scene, camera);
 
-tick()
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
